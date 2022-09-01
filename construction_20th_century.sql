4b11d269f7ebe871cb393c1f4ab6893723bd5cf3
kcpGOAT <90441353+kcpGOAT@users.noreply.github.com>
2022-09-01 14:50:18 -0700

    Create construction_20th_century.sql

---
+++ construction_20th_century.sql
@@ -0,0 +1,10 @@
+SELECT  
+	year_built, 
+    COUNT(*) AS num_built
+    FROM 
+        nyc_sales
+    WHERE 
+        year_built BETWEEN 1900 AND 1999
+    GROUP BY
+        year_built
+ORDER BY num_built DESC;
