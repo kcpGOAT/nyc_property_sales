2ae23cb5e6e43e1e8a4353bc71a2a6b062e94e8b
kcpGOAT <90441353+kcpGOAT@users.noreply.github.com>
2022-09-10 18:38:02 -0700

    Create most-expensive-neighborhoods-by-borough.sql

---
+++ data-analysis/most-expensive-neighborhoods-by-borough.sql
@@ -0,0 +1,31 @@
+CREATE TABLE borough_nb_raw AS
+	SELECT 
+     	borough, 
+     	neighborhood, 
+     	ROUND(AVG(sale_price), 2) AS sale_price
+     	FROM 
+     		nyc_sales
+        WHERE 
+        	year_built > 2000 AND
+            sale_price > 0
+     	GROUP BY 
+     		borough, 
+     		neighborhood;
+
+
+CREATE TABLE borough_nb_ranks AS
+    SELECT 
+        borough, 
+        neighborhood,
+        sale_price, 
+        RANK() OVER 
+            (PARTITION BY borough
+             ORDER BY sale_price DESC)
+        FROM 
+            borough_nb_raw;
+
+SELECT * 
+	FROM 
+    	borough_nb_ranks
+    WHERE 
+    	rank <= 5;
