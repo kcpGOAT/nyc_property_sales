ce8db10e4f02eccc5592f9ed65a18fb975dc9f52
kcpGOAT <90441353+kcpGOAT@users.noreply.github.com>
2022-09-14 14:35:15 -0700

    Update and rename building-class-21st-century to building-class-21st-century.sql

---
+++ data-analysis/building-class-21st-century.sql
@@ -0,0 +1,12 @@
+SELECT
+	building_class_category, 
+	COUNT(building_class_category) AS num_built
+    FROM 
+    	nyc_sales
+    WHERE 
+    	year_built >= 2000 AND
+        sale_price > 0
+    GROUP BY 
+    	building_class_category
+ORDER BY 
+	num_built DESC;
